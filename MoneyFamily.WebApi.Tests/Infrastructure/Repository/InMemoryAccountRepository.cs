74c1dcd0a5999b5563cf0ca93085b2008e8ec204
wataru0407 <wataru.tsukada47@gmail.com>
2022-07-10 10:48:10 +0900

    MF-7 Accountサービスのインメモリリポジトリをリファクタ

--- MoneyFamily.WebApi.Tests/Infrastructure/Repository/InMemoryAccountRepository.cs
+++ MoneyFamily.WebApi.Tests/Infrastructure/Repository/InMemoryAccountRepository.cs
@@ -7,40 +7,45 @@ namespace MoneyFamily.WebApi.Infrastructure.Repository
 {
     public class InMemoryAccountRepository : IAccountRepository
     {
-        public AccountId AccountId1 { get; set; } = new AccountId(Guid.NewGuid());
-        public AccountId AccountId2 { get; set; } = new AccountId(Guid.NewGuid());
-        public AccountId AccountId3 { get; set; } = new AccountId(Guid.NewGuid());
+        public AccountId AccountId1 { get; set; }
+        public AccountId AccountId2 { get; set; }
+        public AccountId AccountId3 { get; set; }
         public UserId UserId1 { get; set; }
         public UserId UserId2 { get; set; }
         public UserId UserId3 { get; set; }
         private readonly List<AccountDto> accounts;
-        private readonly List<AccountUserRelationDto> relations;
+        private readonly List<AccountUserRelationDto> accountUserRelations;
 
         public InMemoryAccountRepository(InMemoryUserRepository userRepository)
         {
+            AccountId1 = new AccountId(Guid.NewGuid());
+            AccountId2 = new AccountId(Guid.NewGuid());
+            AccountId3 = new AccountId(Guid.NewGuid());
+
             UserId1 = userRepository.userId1;
             UserId2 = userRepository.userId2;
             UserId3 = userRepository.userId3;
 
-            var accountDto1 = new AccountDto() { AccountId = AccountId1.Value, AccountName = "test1", CreatedUserId = UserId1.Value };
-            var accountDto2 = new AccountDto() { AccountId = AccountId2.Value, AccountName = "test2", CreatedUserId = UserId2.Value };
-            var accountDto3 = new AccountDto() { AccountId = AccountId3.Value, AccountName = "test3", CreatedUserId = UserId3.Value };
-            accounts = new List<AccountDto> { accountDto1, accountDto2, accountDto3 };
+            accounts = new List<AccountDto> {
+                new AccountDto() { AccountId = AccountId1.Value, AccountName = "test1", CreatedUserId = UserId1.Value },
+                new AccountDto() { AccountId = AccountId2.Value, AccountName = "test2", CreatedUserId = UserId2.Value },
+                new AccountDto() { AccountId = AccountId3.Value, AccountName = "test3", CreatedUserId = UserId3.Value },
+            };
 
-            var relation1 = new AccountUserRelationDto() { AccountId = AccountId1.Value, UserId = UserId1.Value };
-            var relation2 = new AccountUserRelationDto() { AccountId = AccountId2.Value, UserId = UserId1.Value };
-            var relation3 = new AccountUserRelationDto() { AccountId = AccountId2.Value, UserId = UserId2.Value };
-            var relation4 = new AccountUserRelationDto() { AccountId = AccountId3.Value, UserId = UserId1.Value };
-            var relation5 = new AccountUserRelationDto() { AccountId = AccountId3.Value, UserId = UserId2.Value };
-            var relation6 = new AccountUserRelationDto() { AccountId = AccountId3.Value, UserId = UserId3.Value };
-            relations = new List<AccountUserRelationDto> { relation1, relation2, relation3, relation4, relation5, relation6 };
-
+            accountUserRelations = new List<AccountUserRelationDto> {
+                new AccountUserRelationDto() { AccountId = AccountId1.Value, UserId = UserId1.Value },
+                new AccountUserRelationDto() { AccountId = AccountId2.Value, UserId = UserId1.Value },
+                new AccountUserRelationDto() { AccountId = AccountId2.Value, UserId = UserId2.Value },
+                new AccountUserRelationDto() { AccountId = AccountId3.Value, UserId = UserId1.Value },
+                new AccountUserRelationDto() { AccountId = AccountId3.Value, UserId = UserId2.Value },
+                new AccountUserRelationDto() { AccountId = AccountId3.Value, UserId = UserId3.Value },
+            };
         }
 
         public async Task Save(Account account)
         {
             accounts.Add(ToAccountDto(account));
-            relations.AddRange(ToRelationDto(account));
+            accountUserRelations.AddRange(ToRelationDto(account));
         }
 
         public async Task UpdateAccount(Account account)
@@ -52,15 +57,15 @@ namespace MoneyFamily.WebApi.Infrastructure.Repository
 
         public async Task UpdateMember(Account account)
         {
-            relations.RemoveAll(x => x.AccountId == account.Id.Value);
-            relations.AddRange(ToRelationDto(account));
+            accountUserRelations.RemoveAll(x => x.AccountId == account.Id.Value);
+            accountUserRelations.AddRange(ToRelationDto(account));
         }
 
         public async Task Delete(Account account)
         {
             var deleteAccount = accounts.SingleOrDefault(x => x.AccountId == account.Id.Value);
             accounts.Remove(deleteAccount);
-            relations.RemoveAll(x => x.AccountId == account.Id.Value);
+            accountUserRelations.RemoveAll(x => x.AccountId == account.Id.Value);
         }
 
         public async Task<Account?> FindById(AccountId id)
@@ -68,15 +73,15 @@ namespace MoneyFamily.WebApi.Infrastructure.Repository
             var accountDto = accounts.FirstOrDefault(x => x.AccountId.Equals(id.Value));
             if (accountDto == null) return null;
 
-            var relationDtos = relations.Where(x => x.AccountId == id.Value).ToList();
+            var relationDtos = accountUserRelations.Where(x => x.AccountId == id.Value).ToList();
             return ToDomainModel(accountDto, relationDtos);
         }
 
         public async Task<IEnumerable<Account>> GetAll(UserId userId)
         {
-            var relationDtos = relations.Where(x => x.UserId == userId.Value).ToList();
+            var relations = accountUserRelations.Where(x => x.UserId == userId.Value).ToList();
             var accounts = new List<Account>();
-            foreach (var relation in relationDtos)
+            foreach (var relation in relations)
             {
                 var id = new AccountId(relation.AccountId);
                 var account = await FindById(id);
