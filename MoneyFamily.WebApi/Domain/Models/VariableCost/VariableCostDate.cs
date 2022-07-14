80563f7afa9e3d50ccce6d6ffb36b01ecd4ddca2
wataru0407 <wataru.tsukada47@gmail.com>
2022-07-14 23:58:43 +0900

    MF-46 変動費日付の値オブジェクトを作成

---
+++ MoneyFamily.WebApi/Domain/Models/VariableCost/VariableCostDate.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace MoneyFamily.WebApi.Domain.Models.VariableCost
+{
+    public record VariableCostDate
+    {
+        public DateTime DateTime { get; }
+        public int Year { get; }
+        public int Month { get; }
+        public int Day { get; }
+        public int DayOfWeek { get; }
+        public int WeekOfYear { get; }
+
+        public VariableCostDate(DateTime dateTime)
+        {
+            DateTime = dateTime;
+            Year = dateTime.Year;
+            Month = dateTime.Month;
+            Day = dateTime.Day;
+            DayOfWeek = (int)dateTime.DayOfWeek;
+        }
+
+    }
+}
