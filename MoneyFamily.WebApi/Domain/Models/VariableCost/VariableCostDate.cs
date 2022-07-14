e21893048198bc2e43b50f8019a5224a8f04ffeb
wataru0407 <wataru.tsukada47@gmail.com>
2022-07-15 00:19:09 +0900

    MF-46 変動費日付の値オブジェクトで曜日・週番号を設定する

--- MoneyFamily.WebApi/Domain/Models/VariableCost/VariableCostDate.cs
+++ MoneyFamily.WebApi/Domain/Models/VariableCost/VariableCostDate.cs
@@ -4,11 +4,17 @@ namespace MoneyFamily.WebApi.Domain.Models.VariableCost
 {
     public record VariableCostDate
     {
+        // 日付
         public DateTime DateTime { get; }
+        // 年
         public int Year { get; }
+        // 月
         public int Month { get; }
+        // 日にち
         public int Day { get; }
+        // 曜日
         public int DayOfWeek { get; }
+        // 週番号
         public int WeekOfYear { get; }
 
         public VariableCostDate(DateTime dateTime)
@@ -17,7 +23,12 @@ namespace MoneyFamily.WebApi.Domain.Models.VariableCost
             Year = dateTime.Year;
             Month = dateTime.Month;
             Day = dateTime.Day;
-            DayOfWeek = (int)dateTime.DayOfWeek;
+            
+            //月曜を0、日曜を6とする
+            DayOfWeek = ((int)dateTime.DayOfWeek + 6) % 7;
+
+            // 月曜始まりで1年のなかで何番目の週かを設定する
+            WeekOfYear = DateTimeFormatInfo.CurrentInfo.Calendar.GetWeekOfYear(dateTime, CalendarWeekRule.FirstFullWeek, System.DayOfWeek.Monday);
         }
 
     }
